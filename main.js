7512de119ca991c9fc565c48a95678d7a294fcf0
adamel2 <elddawork@gmail.com>
2021-03-28 18:24:06 +0300

    add sweet alert v2

--- main.js
+++ main.js
@@ -6,32 +6,40 @@ function addNote(event) {
   let time = document.getElementById("time-input").value;
   let fullDate = Date.parse(date + " " + time);
   //Validation
-  (newNote === "" || newNote.trim() == '') && alert("Please enter your task details!");{
-  (fullDate < new Date().getTime()) && alert("Please enter correct date !");
-  (fullDate === null) &&  alert("The date is empty!");
-  } ((newNote != "" && newNote !== null) && (fullDate >= new Date().getTime()) && (fullDate != null) ) && (newNote.trim() != '') &&
+  (newNote === "" || newNote.trim() == "") &&
+    Swal.fire("Error!", "Please enter your task details!", "error");
+  {
+    fullDate < new Date().getTime() &&
+      Swal.fire("Error!", "Please enter correct date !", "error");
+    fullDate === null && Swal.fire("Error!", "The date is empty!", "error");
+  }
+  newNote != "" &&
+    newNote !== null &&
+    fullDate >= new Date().getTime() &&
+    fullDate != null &&
+    newNote.trim() != "" &&
     localStorage.setItem(
       key,
       JSON.stringify({ date: fullDate, title: newNote.toString() })
     );
-    document.getElementById(
-      "list"
-    ).innerHTML += `<li id=${key} onmouseover="unHidden(this.id)" onmouseout="appendHiddenAttribute(this.id)"><div class="item-div">${
-      JSON.parse(localStorage.getItem(key)).title
-    } </div><input class="input-date" type="text" disabled value=${
-      new Date(
-        JSON.parse(localStorage.getItem(key)).date
-      ).toLocaleDateString() +
-      "-" +
-      new Date(JSON.parse(localStorage.getItem(key)).date).toTimeString()
-    }> <button id=${key} type="button" class="btn btn-warning btn-add btn-place" onclick="removeItemFromUnOrderList(this.id)" hidden>
+  document.getElementById(
+    "list"
+  ).innerHTML += `<li id=${key} onmouseover="unHidden(this.id)" onmouseout="appendHiddenAttribute(this.id)"><div class="item-div">${
+    JSON.parse(localStorage.getItem(key)).title
+  } </div><input class="input-date" type="text" disabled value=${
+    new Date(JSON.parse(localStorage.getItem(key)).date).toLocaleDateString() +
+    "-" +
+    new Date(JSON.parse(localStorage.getItem(key)).date).toTimeString()
+  }> <button id=${key} type="button" class="btn btn-warning btn-add btn-place" onclick="removeItemFromUnOrderList(this.id)" hidden>
                 <span class="glyphicon glyphicon-remove"></span>
             </button></li>`;
-    document.getElementById("content-add").value = ``;
-  }
+  document.getElementById("content-add").value = ``;
+  Swal.fire("Good job!", "Thank you for adding a new task", "success");
+}
 //Clear Local Storage
-function clearSession() {
+function clearStorage() {
   localStorage.clear();
+  Swal.fire("Carefully!", "All The tasks are removed!", "warning");
   window.onload();
 }
 //Remove item list by id;
@@ -44,7 +52,10 @@ window.onload = function () {
   (keys = Object.keys(localStorage)), (indexKey = keys.length);
   document.getElementById("list").innerHTML = "";
   while (indexKey--) {
-    if (JSON.parse(localStorage.getItem(keys[indexKey])).date < new Date().getTime()) {
+    if (
+      JSON.parse(localStorage.getItem(keys[indexKey])).date <
+      new Date().getTime()
+    ) {
       localStorage.removeItem(keys[indexKey]);
     } else {
       document.getElementById("list").innerHTML += `<li id=${
@@ -56,7 +67,9 @@ window.onload = function () {
           JSON.parse(localStorage.getItem(keys[indexKey])).date
         ).toLocaleDateString() +
         "-" +
-        new Date(JSON.parse(localStorage.getItem(keys[indexKey])).date).toTimeString()
+        new Date(
+          JSON.parse(localStorage.getItem(keys[indexKey])).date
+        ).toTimeString()
       }> <button id=${
         keys[indexKey]
       } type="button" class="btn btn-warning btn-add btn-place" onclick="removeItemFromUnOrderList(this.id)" hidden>
@@ -81,14 +94,14 @@ function uuidV4() {
   );
 }
 //Remove hidden attribute from button
-function unHidden(id){
-    let myItem = document.getElementById(id);
-    let buttonChild = myItem.querySelector("button");
-    buttonChild.removeAttribute("hidden");
+function unHidden(id) {
+  let myItem = document.getElementById(id);
+  let buttonChild = myItem.querySelector("button");
+  buttonChild.removeAttribute("hidden");
 }
-// add hidden attribute to button
-function appendHiddenAttribute(id){
-    let myItem = document.getElementById(id);
-    let buttonChild = myItem.querySelector("button");
-    buttonChild.setAttribute("hidden","");
+// Add hidden attribute to button
+function appendHiddenAttribute(id) {
+  let myItem = document.getElementById(id);
+  let buttonChild = myItem.querySelector("button");
+  buttonChild.setAttribute("hidden", "");
 }
